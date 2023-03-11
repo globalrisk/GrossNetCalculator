df0485015b4fc854ec10cd79c1e83d10acc4f18d
VuTrong <globalrisk07@gmail.com>
2023-03-11 18:28:11 +0700

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrossNetCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
